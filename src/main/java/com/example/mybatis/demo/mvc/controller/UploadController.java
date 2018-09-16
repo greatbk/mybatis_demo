38f54b041e038687346fdc66add55688ebf12b49
greatbk <greatbk@daum.net>
2018-09-16 09:15:13 +0900

    .

--- src/main/java/com/example/mybatis/demo/mvc/controller/UploadController.java
+++ src/main/java/com/example/mybatis/demo/mvc/controller/UploadController.java
@@ -1,12 +1,15 @@
 package com.example.mybatis.demo.mvc.controller;
 
+import com.example.mybatis.demo.mvc.service.UploadService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Map;
 import java.util.Set;
@@ -16,22 +19,26 @@ public class UploadController {
 
     private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
+    @Autowired
+    private UploadService uploadService;
+
     @GetMapping("uploadForm")
     public String uploadForm() {
         return "uploadForm.simple";
     }
 
     @PostMapping("upload")
-    public String upload(@RequestParam MultipartFile file, @RequestParam Map<String, String> param) {
+    public ModelAndView upload(@RequestParam MultipartFile file, @RequestParam Map<String, String> param, ModelAndView model) {
         if(file != null) {
-            logger.debug(file.toString());
+            uploadService.save(file);
         }
         if(param != null) {
             Set<String> keySet = param.keySet();
             for(String key : keySet) {
-                logger.debug(key);
+                logger.debug(String.format("%s:%s", key, param.get(key)));
             }
         }
-        return "upload.simple";
+        model.setViewName("upload.simple");
+        return model;
     }
 }
