a7e26b1f778249fb3fcc9600a382085968d3d1bd
greatbk <greatbk@daum.net>
2018-09-25 22:33:53 +0900

    .

--- src/main/java/com/example/mybatis/demo/exception/AsyncException.java
+++ src/main/java/com/example/mybatis/demo/exception/AsyncException.java
@@ -1,6 +1,6 @@
 package com.example.mybatis.demo.exception;
 
-public class AsyncException extends RuntimeException {
+public class AsyncException extends BaseException {
 
     public AsyncException(String message) {
         super(message);
